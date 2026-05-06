1784d175cb28fddc374f71735b83baae7d8dcdd4
Olloyor Gofurov <olloyorgofurov@Olloyors-MacBook-Air.local>
2026-05-06 13:46:59 +0500

    Remove Netlify config; embed real OAuth credentials directly in oauth-config.js

--- oauth-config.js
+++ oauth-config.js
@@ -1,7 +1,7 @@
 window.OAUTH_CONFIG = {
-  TELEGRAM_BOT_USERNAME: '__TELEGRAM_BOT_USERNAME__',
-  TELEGRAM_BOT_ID:       '__TELEGRAM_BOT_ID__',
-  FACEBOOK_APP_ID:       '__FACEBOOK_APP_ID__',
+  TELEGRAM_BOT_USERNAME: 'securesocialmanager_bot',
+  TELEGRAM_BOT_ID:       '8506396231',
+  FACEBOOK_APP_ID:       '1487980476353532',
   FACEBOOK_SCOPES:       'public_profile,pages_show_list,pages_manage_posts,pages_read_engagement',
   INSTAGRAM_SCOPES:      'public_profile',
 };
